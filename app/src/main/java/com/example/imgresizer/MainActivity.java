ff968e6228439ad836a59d781374a4beeb58d4da
anemia004 <Khorramshahr04@duck.com>
2026-06-22 20:12:42 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/imgresizer/MainActivity.java
+++ app/src/main/java/com/example/imgresizer/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.imgresizer;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
